322ff6e3efe33c06834e0d8fe93c9aa234291669
Rogan Morrow <rogan.morrow@rokt.com>
2017-05-09 20:22:12 +1000

    i dunno

--- src/Whatever/BudgetShopping.hs
+++ src/Whatever/BudgetShopping.hs
@@ -3,14 +3,14 @@ module Whatever.BudgetShopping where
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 
-budgetFold :: [Int] -> [Int] -> Vector Integer -> Int -> Vector Integer
+budgetFold :: [Int] -> [Int] -> Vector Int -> Int -> Vector Int
 budgetFold v w m n = m V.// [(n, mx)]
-  where candidates = [(toInteger v') + (m V.! (n - w'))
+  where candidates = [v' + (m V.! (n - w'))
                      | (v', w') <- zip v w,
                        w' <= n]
         mx | null candidates = 0
            | otherwise = maximum candidates
 
-budgetShopping :: Int -> [Int] -> [Int] -> Integer
+budgetShopping :: Int -> [Int] -> [Int] -> Int
 budgetShopping n v w = m V.! n
   where m = foldl (budgetFold v w) (V.replicate (n+1) 0) [1..n]
