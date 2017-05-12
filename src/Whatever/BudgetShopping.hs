337092cf24f0df93f3c144fd846e047e319611cf
Rogan Morrow <rogan.morrow@rokt.com>
2017-05-12 14:53:25 +1000

    oops

--- src/Whatever/BudgetShopping.hs
+++ src/Whatever/BudgetShopping.hs
@@ -2,9 +2,10 @@ module Whatever.BudgetShopping where
 
 import Data.Vector (Vector)
 import qualified Data.Vector as V
+import Data.List
 
 budgetFold :: [Int] -> [Int] -> Vector Int -> Int -> Vector Int
-budgetFold v w m n = m V.// [(n, mx)]
+budgetFold v w m n = mx `seq` m V.// [(n, seq n mx)]
   where candidates = [v' + (m V.! (n - w'))
                      | (v', w') <- zip v w,
                        w' <= n]
@@ -13,4 +14,4 @@ budgetFold v w m n = m V.// [(n, mx)]
 
 budgetShopping :: Int -> [Int] -> [Int] -> Int
 budgetShopping n v w = m V.! n
-  where m = foldl (budgetFold v w) (V.replicate (n+1) 0) [1..n]
+  where m = foldl' (budgetFold v w) (V.replicate (n+1) 0) [1..n]
